7fd4769e9d2c46f943535505c538384c461b13e0
abhinash <khare.abhinash4@gmail.com>
2017-11-14 18:37:34 +0530

    fix scripts

---
+++ src/test.py
@@ -0,0 +1,5 @@
+import numpy as np
+from data_provider import DataProvider
+import sys
+
+data = DataProvider(sys.argv[1])
--- src/testCNN.sh
+++ src/testCNN.sh
@@ -1,7 +1,7 @@
-python src/train_characters.py cnn chinese5k small 0>cnns0.txt
-python src/train_characters.py cnn chinese5k small 1>cnns1.txt
-python src/train_characters.py cnn chinese5k large 0>cnnl0.txt
-python src/train_characters.py cnn chinese5k large 1>cnnl1.txt
+python src/train_characters.py cnn chinese5k small 0 > cnns0.txt
+python src/train_characters.py cnn chinese5k small 1 > cnns1.txt
+python src/train_characters.py cnn chinese5k large 0 > cnnl0.txt
+python src/train_characters.py cnn chinese5k large 1 > cnnl1.txt
 
 
 
